292b6d367fac12f3870f152b0405171085248685
Ryan Linton <ryanlntn@gmail.com>
2021-04-16 16:56:13 -0600

    add xcodeproj

---
+++ apple/react-native-macos-cursor/react-native-macos-cursor/react_native_macos_cursor.h
@@ -0,0 +1,12 @@
+//
+//  react_native_macos_cursor.h
+//  react-native-macos-cursor
+//
+//  Created by Ryan Linton on 4/16/21.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface react_native_macos_cursor : NSObject
+
+@end
---
+++ apple/react-native-macos-cursor/react-native-macos-cursor/react_native_macos_cursor.m
@@ -0,0 +1,12 @@
+//
+//  react_native_macos_cursor.m
+//  react-native-macos-cursor
+//
+//  Created by Ryan Linton on 4/16/21.
+//
+
+#import "react_native_macos_cursor.h"
+
+@implementation react_native_macos_cursor
+
+@end
