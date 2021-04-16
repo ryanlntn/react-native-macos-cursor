88e5b9337d82868c393349a290d0f20330bd23ef
Ryan Linton <ryanlntn@gmail.com>
2021-04-16 17:28:51 -0600

    fix exception handling

--- apple/RNCursor.m
+++ apple/RNCursor.m
@@ -10,7 +10,7 @@ RCT_EXPORT_METHOD(setCursor: (NSString *) type) {
   @try {
     [[NSCursor valueForKey:[type stringByAppendingString:@"Cursor"]] performSelector:@selector(set)];
   }
-  @catch {
+  @catch (NSException *exception) {
     RCTLogWarn(@"Unrecognized cursor type %@. Expected one of arrow, iBeam, crosshair, closedHand, openHand, pointingHand, resizeLeft, resizeRight, resizeLeftRight, resizeUp, resizeDown, resizeUpDown, disappearingItem, iBeamCursorForVerticalLayout, operationNotAllowed, dragLink, dragCopy, or contextualMenu.", type);
   }
 }
